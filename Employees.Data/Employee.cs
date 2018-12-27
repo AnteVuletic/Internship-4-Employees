38d9e14f81301a46fe559d0b9a9d63c19d9debef
AnteVuletic <antevuletic1995@gmail.com>
2018-12-27 09:34:00 +0100

    Data layer class mockup

---
+++ Employees.Data/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Data
+{
+    public class Employee
+    {
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public string Oib { get; set; }
+        public DateTime DateBirth { get; set; }
+
+        public Employee(string forename,string surname,string oib,DateTime dateBirth)
+        {
+            Forename = forename;
+            Surname = surname;
+            Oib = oib;
+            DateBirth = dateBirth;
+        }
+    }
+}
---
+++ Employees.Data/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Data
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Project(string name,DateTime startDate,DateTime endDate)
+        {
+            Name = name;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
